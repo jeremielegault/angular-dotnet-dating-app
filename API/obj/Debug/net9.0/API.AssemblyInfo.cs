5d8ea5d3242007e35d5a1b25d461eb95684e6b29
Jeremie Legault <jeremie.legault@avanade.com>
2025-10-27 11:25:37 -0400

    save the api assembly

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b19727dbb0d5a429f6000836f05dfd3b7594352d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4d90c80c3109c66a4def61341535265c43e3515")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
