b19727dbb0d5a429f6000836f05dfd3b7594352d
Jeremie Legault <jeremie.legault@avanade.com>
2025-10-27 09:56:55 -0400

    database migration

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4de1087b6711f79728170155c763333b19b8e46e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a85b89226635d8ba4db99d510b942074a8cafc0c")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
