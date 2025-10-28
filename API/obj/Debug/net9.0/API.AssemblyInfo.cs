ec6a316c63a69f8be23556b343d19e5496ef6056
Jeremie Legault <jeremie.legault@avanade.com>
2025-10-28 09:20:27 -0400

    add validation to api for required fields

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da7326c833b75c3d30af76eecfa3850e9ed36c04")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de8bcdde9b5073cc8b63ed902b23f5e963aac4bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
