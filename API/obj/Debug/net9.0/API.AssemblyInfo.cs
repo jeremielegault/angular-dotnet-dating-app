dc9588c21dad137c598d245bcb652dd7b9b893fb
Jeremie Legault <jeremie.legault@avanade.com>
2025-10-28 14:53:43 -0400

    add some styling to basae angular app using daisy ui and tailwind

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a16b8cc9427ffb4c860c13818481cf545d5e9616")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10fa5fb865e8d7c245983d7d37fa3c1930a94383")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
