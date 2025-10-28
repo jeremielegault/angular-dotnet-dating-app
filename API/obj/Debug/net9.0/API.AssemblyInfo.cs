203640917762f797fa505ab3461488502de0157d
Jeremie Legault <jeremie.legault@avanade.com>
2025-10-28 15:26:37 -0400

    implement account service and add logging in

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10fa5fb865e8d7c245983d7d37fa3c1930a94383")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f0ff0cd1a01aa0625d9ae47a005c994a8dbee1b")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
