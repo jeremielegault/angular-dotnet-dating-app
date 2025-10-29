eb47c9c6f99d2102e1aa7bc1c88a916e5580248b
Jeremie Legault <jeremie.legault@avanade.com>
2025-10-29 09:47:23 -0400

    use signals to pass data from child to parent and parent to child

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e5647ebbdb9edf6c5e4f4dec81d81de0f6d252a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0fab6e84bbfe4536a6466f12c338084ed45c647b")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- client/src/features/account/register/register.ts
+++ client/src/features/account/register/register.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, input, output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RegisterCreds } from '../../../types/user';
+import { RegisterCreds, User } from '../../../types/user';
 
 @Component({
   selector: 'app-register',
@@ -9,12 +9,15 @@ import { RegisterCreds } from '../../../types/user';
   styleUrl: './register.css',
 })
 export class Register {
+  membersFromHome = input.required<User[]>()
+  cancelRegister = output<boolean>()
   protected creds = {} as RegisterCreds;
+
   register() {
     console.log(this.creds)
   }
 
   cancel() {
-    console.log('cancel')
+    this.cancelRegister.emit(false)
   }
 }
