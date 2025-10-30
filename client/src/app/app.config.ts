9d43e5071e8b1ad0f85738df8be0b7ad20fd6cc1
Jeremie Legault <jeremie.legault@avanade.com>
2025-10-30 10:22:09 -0400

    add UI tests to ensure that error validation works as expected

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -3,15 +3,16 @@ import { ApplicationConfig, provideAppInitializer, provideBrowserGlobalErrorList
 import { provideRouter, withViewTransitions } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { lastValueFrom } from 'rxjs';
+import { errorInterceptor } from '../core/interceptors/error-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideRouter(routes, withViewTransitions()),
-    provideHttpClient(),
+    provideHttpClient(withInterceptors([errorInterceptor])),
     provideAppInitializer(async () => {
       const initService = inject(InitService)
       return new Promise<void>((resolve) => {
