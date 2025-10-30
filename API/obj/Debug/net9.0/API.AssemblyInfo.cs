34948fac5d0e62280b804214d0928d646e6cea84
Jeremie Legault <jeremie.legault@avanade.com>
2025-10-30 11:45:58 -0400

    database schema setup

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f599686f8b63127abb413e731eb14ed149e6644")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32b7da17396a69119ef70071de116faa9186c718")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
