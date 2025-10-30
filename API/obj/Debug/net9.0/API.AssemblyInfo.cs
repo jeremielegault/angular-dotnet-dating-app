5f599686f8b63127abb413e731eb14ed149e6644
Jeremie Legault <jeremie.legault@avanade.com>
2025-10-30 11:40:00 -0400

    add entities for members and photos

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da190edc274c126228c37789ef069bc42b749317")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f3dd3f95a8c1be76b54adcf55edeba3d43aee87")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
