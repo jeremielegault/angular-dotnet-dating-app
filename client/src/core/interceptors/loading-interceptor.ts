5b8539f41e7eabed72bd0008a215a1f403643989
Jeremie Legault <jeremie.legault@avanade.com>
2025-11-03 15:09:21 -0500

    implement simple caching to improve ui performance

--- client/src/core/interceptors/loading-interceptor.ts
+++ client/src/core/interceptors/loading-interceptor.ts
@@ -1,16 +1,26 @@
 import { inject } from '@angular/core';
 import { BusyService } from './../services/busy-service';
-import { HttpInterceptorFn } from '@angular/common/http';
-import { delay, finalize } from 'rxjs';
+import { HttpEvent, HttpInterceptorFn } from '@angular/common/http';
+import { delay, finalize, of, tap } from 'rxjs';
 
-const cache = new Map<string, any>()
+const cache = new Map<string, HttpEvent<unknown>>()
 
 export const loadingInterceptor: HttpInterceptorFn = (req, next) => {
   const busyService = inject(BusyService)
 
+  if (req.method === 'GET') {
+    const cachedResponse = cache.get(req.url)
+    if (cachedResponse) {
+      return of(cachedResponse)
+    }
+  }
+
   busyService.busy()
   return next(req).pipe(
     delay(500),
+    tap(response => {
+      cache.set(req.url, response)
+    }),
     finalize(() => {
       busyService.idle()
     })
