6c2c676d4f8eaac7e83945dd9242aa6ad07348bf
Jeremie Legault <jeremie.legault@avanade.com>
2025-11-03 16:20:05 -0500

    implementing a service for photo uplaod and an interface

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb84af7ba49a1933ad53bb5d97487b967cf0e21d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89051edf691663f4ce1934fe974100c96d71ecec")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
