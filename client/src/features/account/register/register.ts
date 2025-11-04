2856f3e3b3f65c40a74caf7d0704d68e0f27e069
Jeremie Legault <jeremie.legault@avanade.com>
2025-11-04 15:18:27 -0500

    reuse text input component

--- client/src/features/account/register/register.ts
+++ client/src/features/account/register/register.ts
@@ -3,10 +3,11 @@ import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, Validatio
 import { RegisterCreds } from '../../../types/user';
 import { AccountService } from '../../../core/services/account-service';
 import { JsonPipe } from '@angular/common';
+import { TextInput } from '../../../shared/text-input/text-input';
 
 @Component({
   selector: 'app-register',
-  imports: [ReactiveFormsModule, JsonPipe],
+  imports: [ReactiveFormsModule, JsonPipe, TextInput],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
@@ -21,11 +22,11 @@ export class Register implements OnInit {
   }
   initializeForm() {
     this.registerForm = new FormGroup({
-      email: new FormControl('johndoe@test.com', [Validators.required, Validators.email]),
+      email: new FormControl('', [Validators.required, Validators.email]),
       displayName: new FormControl('', Validators.required),
       password: new FormControl('', [Validators.required,
       Validators.minLength(4), Validators.maxLength(8)]),
-      confirmPassword: new FormControl('', [Validators.required, this.matchValues('password')])
+      confirmPassword: new FormControl('', [Validators.required, this.matchValues('password'), Validators.minLength(4), Validators.maxLength(8)])
     });
     this.registerForm.controls['password'].valueChanges.subscribe(() => {
       this.registerForm.controls['confirmPassword'].updateValueAndValidity()
