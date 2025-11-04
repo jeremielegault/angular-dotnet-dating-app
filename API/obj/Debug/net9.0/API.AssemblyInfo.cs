a98adbf92a9151e325c089f58ef396a0de14baec
Jeremie Legault <jeremie.legault@avanade.com>
2025-11-04 16:14:55 -0500

    accomodate new fields on the api

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a677cdeb209c24bee72cd7b9e002f88fc5d977f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be62df1c040c635448a323ee95575f220deedbc0")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
