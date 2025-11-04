06ed0721efadf806b612ebb77d59d9d4245c5696
Jeremie Legault <jeremie.legault@avanade.com>
2025-11-04 09:55:37 -0500

    images can be clicked and dragged into the upload field

--- client/src/features/members/member-photos/member-photos.ts
+++ client/src/features/members/member-photos/member-photos.ts
@@ -1,5 +1,5 @@
 import { Photo } from './../../../types/member';
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { MemberService } from '../../../core/services/member-service';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
@@ -12,14 +12,33 @@ import { ImageUpload } from "../../../shared/image-upload/image-upload";
   templateUrl: './member-photos.html',
   styleUrl: './member-photos.css',
 })
-export class MemberPhotos {
+export class MemberPhotos implements OnInit {
   protected memberService = inject(MemberService)
   private route = inject(ActivatedRoute)
-  protected photos$?: Observable<Photo[]>
-  constructor() {
+  protected photos = signal<Photo[]>([])
+  protected loading = signal(false)
+
+  ngOnInit(): void {
     const memberId = this.route.parent?.snapshot.paramMap.get('id')
     if (memberId) {
-      this.photos$ = this.memberService.getMemberPhotos(memberId)
+      this.memberService.getMemberPhotos(memberId).subscribe({
+        next: photos => this.photos.set(photos)
+      })
     }
+  }
+
+  onUploadImage(file: File) {
+    this.loading.set(true)
+    this.memberService.uploadPhoto(file).subscribe({
+      next: photo => {
+        this.memberService.editMode.set(false)
+        this.loading.set(false)
+        this.photos.update(photos => [...photos, photo])
+      },
+      error: error => {
+        console.log('Error uploading image: ', error)
+        this.loading.set(false)
+      }
+    })
   }
 }
