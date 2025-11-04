e4c59aec7c9001e9dc5d2b51a002eec6c42bc979
Jeremie Legault <jeremie.legault@avanade.com>
2025-11-04 16:41:24 -0500

    finish up register flow

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be62df1c040c635448a323ee95575f220deedbc0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a98adbf92a9151e325c089f58ef396a0de14baec")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- client/src/features/account/register/register.ts
+++ client/src/features/account/register/register.ts
@@ -2,23 +2,25 @@ import { Component, inject, output, signal } from '@angular/core';
 import { AbstractControl, FormBuilder, FormGroup, ReactiveFormsModule, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 import { RegisterCreds } from '../../../types/user';
 import { AccountService } from '../../../core/services/account-service';
-import { JsonPipe } from '@angular/common';
 import { TextInput } from '../../../shared/text-input/text-input';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  imports: [ReactiveFormsModule, JsonPipe, TextInput],
+  imports: [ReactiveFormsModule, TextInput],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
 export class Register {
   private accountService = inject(AccountService)
+  private router = inject(Router)
   private fb = inject(FormBuilder)
   cancelRegister = output<boolean>()
   protected creds = {} as RegisterCreds;
   protected credentialsForm: FormGroup;
   protected profileForm: FormGroup;
   protected currentStep = signal(1)
+  protected validationErrors = signal<string[]>([])
 
   constructor() {
     this.credentialsForm = this.fb.group({
@@ -34,7 +36,7 @@ export class Register {
     })
 
     this.profileForm = this.fb.group({
-      gender: ['', Validators.required],
+      gender: ['male', Validators.required],
       dateOfBirth: ['', Validators.required],
       city: ['', Validators.required],
       country: ['', Validators.required],
@@ -70,15 +72,18 @@ export class Register {
   register() {
     if (this.profileForm.valid && this.credentialsForm.valid) {
       const formData = { ...this.profileForm.value, ...this.credentialsForm.value }
-      console.log('FORM DATA', formData)
+      this.accountService.register(formData).subscribe({
+        next: () => {
+          this.router.navigateByUrl('/members')
+          this.cancel()
+        },
+        error: error => {
+          console.log(error)
+          this.validationErrors.set(error)
+        }
+      })
     }
-    // this.accountService.register(this.creds).subscribe({
-    //   next: response => {
-    //     console.log(response)
-    //     this.cancel()
-    //   },
-    //   error: error => console.log(error)
-    // })
+
   }
 
   cancel() {
