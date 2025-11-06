1e0179527d08f75b63a1ebe4efc7af642bad67cb
Jeremie Legault <jeremie.legault@avanade.com>
2025-11-06 10:17:51 -0500

    add client side filtering

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbbd37c5ae259795de77e9c80602dfdc72f6d877")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5ad7f62e3a5fd918535da0844aa4d1284174c96")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
