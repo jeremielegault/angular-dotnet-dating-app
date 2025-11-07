96139a9790f907480d944bbcd797c69fa3ad7b18
Jeremie Legault <jeremie.legault@avanade.com>
2025-11-07 11:45:51 -0500

    display like button on member cards and fill it in if the user was previousy liked

--- client/src/core/services/account-service.ts
+++ client/src/core/services/account-service.ts
@@ -3,12 +3,14 @@ import { inject, Injectable, signal } from '@angular/core';
 import { LoginCreds, RegisterCreds, User } from '../../types/user';
 import { tap } from 'rxjs';
 import { environment } from '../../environments/environment';
+import { LikesService } from './likes-service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AccountService {
   private http = inject(HttpClient);
+  private likesService = inject(LikesService)
   currentUser = signal<User | null>(null);
   private baseUrl = environment.apiUrl;
 
@@ -24,6 +26,7 @@ export class AccountService {
   setCurrentUser(user: User) {
     localStorage.setItem('user', JSON.stringify(user))
     this.currentUser.set(user);
+    this.likesService.getLikeIds()
   }
   login(creds: LoginCreds) {
     return this.http.post<User>(this.baseUrl + 'account/login', creds).pipe(
@@ -37,6 +40,7 @@ export class AccountService {
   logout() {
     localStorage.removeItem('user')
     localStorage.removeItem('filters')
+    this.likesService.clearLikeIds()
     this.currentUser.set(null);
   }
 }
