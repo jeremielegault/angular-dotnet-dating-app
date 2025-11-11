12ff7e652f4f27249d9a1ecbd1f6a7078d3ce8c5
Jeremie Legault <jeremie.legault@avanade.com>
2025-11-11 11:27:06 -0500

    get messages to show up on template client side

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f35bd592911297d7a8d3aae0564ed7c1e128a3f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+009fffc55f6686778edeaec44ac53a7b5731aede")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ client/src/core/services/message-service.ts
@@ -0,0 +1,22 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { PaginatedResult } from '../../types/pagination';
+import { Message } from '../../types/message';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MessageService {
+  private http = inject(HttpClient)
+  private baseUrl = environment.apiUrl;
+
+  getMessages(container: string, pageNumber: number, pageSize: number) {
+    let params = new HttpParams()
+    params = params.append('pageNumber', pageNumber)
+    params = params.append('pageSize', pageSize)
+    params = params.append('container', container)
+
+    return this.http.get<PaginatedResult<Message>>(this.baseUrl + 'messages', { params })
+  }
+}
